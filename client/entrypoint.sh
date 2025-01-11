c8470f445449add89978dfc8603be0c046fdb12f
Marcel Ritter <marcel+git@linux-ng.de>
2025-01-11 11:04:10 +0100

    Simplify client settings

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -6,14 +6,6 @@ ZOTERO_CONFIG_FILE="$ROOT_DIR/resource/config.js"
 cd $ROOT_DIR
 
 # Configure
-if [ $SERVER_SECURE_PROTOCOL -eq 0 ]; then
-	DATA_SERVER_ADDRESS="http://$SERVER_HOST:$DATA_SERVER_PORT"
-	STREAM_SERVER_ADDRESS="ws://$SERVER_HOST:$STREAM_SERVER_PORT"
-else
-	DATA_SERVER_ADDRESS="https://$SERVER_HOST:$DATA_SERVER_PORT"
-	STREAM_SERVER_ADDRESS="wss://$SERVER_HOST:$STREAM_SERVER_PORT"
-fi
-
 sed -i "s#https://api.zotero.org/#$DATA_SERVER_ADDRESS/#g" $ZOTERO_CONFIG_FILE
 sed -i "s#wss://stream.zotero.org/#$STREAM_SERVER_ADDRESS/#g" $ZOTERO_CONFIG_FILE
 sed -i "s#https://zoteroproxycheck.s3.amazonaws.com/test##g" $ZOTERO_CONFIG_FILE
