2e497d10edbe5788d4108bd8af93008a32028edf
Marcel Ritter <marcel+git@linux-ng.de>
2025-01-15 12:47:02 +0100

    Now really apply permission (not just show SQL)

--- dataserver/scripts/add-key.sh
+++ dataserver/scripts/add-key.sh
@@ -26,14 +26,14 @@ if [ "$libraryID" == "" ]; then
 fi
 echo "libraryID: $libraryID"
 
-echo "INSERT INTO keys (key, userID, name) VALUES ('${KEY}', '${userID}', 'Automatic Zotero Client Key')"
 keyID=$(echo "INSERT INTO \`keys\` (\`key\`, userID, name) VALUES ('${KEY}', '${userID}', 'Automatic Zotero Client Key'); SELECT LAST_INSERT_ID()" | $MYSQL zotero_master -s -N)
 
 echo "keyID: $keyID"
 
 for PERMISSION in 'library' 'write'; do
-	echo "INSERT INTO keyPermissions (keyID, libraryID, permission, granted) VALUES (${keyID},0,'${PERMISSION}',1)"
+	echo "INSERT INTO keyPermissions (keyID, libraryID, permission, granted) VALUES (${keyID},0,'${PERMISSION}',1)" | $MYSQL zotero_master
 done
 for PERMISSION in 'library' 'notes' 'write'; do
-	echo "INSERT INTO keyPermissions (keyID, libraryID, permission, granted) VALUES (${keyID},${libraryID},'${PERMISSION}',1)"
+	echo "INSERT INTO keyPermissions (keyID, libraryID, permission, granted) VALUES (${keyID},${libraryID},'${PERMISSION}',1)" | $MYSQL zotero_master
+
 done
