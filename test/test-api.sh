6a615f8717bffe13040400e91736e2c56c8676e5
Marcel Ritter <marcel+git@linux-ng.de>
2025-01-16 16:33:38 +0100

    Added some tests - pay attention: they do modify objects!

--- test/test-api.sh
+++ test/test-api.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-# zotero.org
-API_KEY=SKvLFJ3pavlc4CAEFT5gYZUB
-USERID=16126166
-API_URL=https://api.zotero.org
-
-# Self-hosted
+# Read settings from setup
 . ../.env
-API_KEY=${TESTUSER_KEY}
-USERID=${TESTUSER_ID}
-API_URL=${DATA_SERVER_URL}
 
-curl -H "Zotero-API-Key: ${API_KEY}" "${API_URL}/users/${USERID}/items"
-curl -H "Zotero-API-Key: ${API_KEY}" "${API_URL}/users/${USERID}/collections?direction=desc&format=json&limit=100&sort=dateModified"
+echo "API URL: ${DATA_SERVER_URL}"
+echo "User name: ${TESTUSER_NAME}"
+echo "User API key: ${TESTUSER_KEY}"
+echo "User ID: ${TESTUSER_ID}"
+
+# API-key based checks:
+echo -n "List user's items...      "
+curl --fail-with-body --silent -H "Zotero-API-Key: ${TESTUSER_KEY}" "${DATA_SERVER_URL}/users/${TESTUSER_ID}/items"  >& /dev/null\
+	&& echo "Success" || echo "Failed"
+
+echo -n "List user's collections..."
+curl --fail-with-body --silent -H "Zotero-API-Key: ${TESTUSER_KEY}" "${DATA_SERVER_URL}/users/${TESTUSER_ID}/collections?direction=desc&format=json&limit=100&sort=dateModified" >& /dev/null \
+	&& echo "Success" || echo "Failed"
+
+echo -n "Add new API key...        "
+# Username/password based checks:
+curl --fail-with-body --silent \
+   -X POST \
+   -H 'Content-Type: application/json' \
+   -H 'Zotero-API-Version: 3' \
+   -H 'Zotero-Schema-Version: 32' \
+   -d "{ \"username\":\"${TESTUSER_NAME}\", \"password\":\"${TESTUSER_PASSWORD}\", \"name\":\"Automatic Zotero Client Key\", \"access\":{\"user\":{\"library\":true,\"notes\":true,\"write\":true,\"files\":true}, \"groups\":{\"all\":{\"library\":true,\"write\":true}}} }" \
+    "${DATA_SERVER_URL}/keys" >& /dev/null \
+    && echo "Success" || echo "Failed"
