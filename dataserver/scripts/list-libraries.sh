894983c18bc8a23b5250bcb8311b0d020754cfb2
Marcel Ritter <marcel+git@linux-ng.de>
2025-01-17 19:47:04 +0100

    List all libraries

--- dataserver/scripts/list-libraries.sh
+++ dataserver/scripts/list-libraries.sh
@@ -10,3 +10,19 @@ echo 'SELECT * FROM shardLibraries;' | $MYSQL zotero_shard_1
 
 echo "# Libraries in zotero_shard_2:"
 echo 'SELECT * FROM shardLibraries;' | $MYSQL zotero_shard_2
+
+echo "# List all libraries:"
+echo   '(SELECT libraryID,libraryType,lastUpdated,version,storageUsage FROM zotero_shard_1.shardLibraries)
+	UNION
+	(SELECT libraryID,libraryType,lastUpdated,version,storageUsage FROM zotero_shard_2.shardLibraries)
+	ORDER BY libraryID;' | $MYSQL
+
+echo "# List all user libraries (zotero_shard_1):"
+echo   'SELECT * FROM zotero_shard_1.shardLibraries AS shardLibs, zotero_master.users AS us
+	WHERE shardLibs.libraryID = us.libraryID
+	ORDER BY us.libraryID;' | $MYSQL
+
+echo "# List all group libraries (zotero_shard_2):"
+echo   'SELECT * FROM zotero_shard_2.shardLibraries AS shardLibs, zotero_master.groups AS gr
+	WHERE shardLibs.libraryID = gr.libraryID
+	ORDER BY gr.libraryID;' | $MYSQL
