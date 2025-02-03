ef0536c057341456df757f1c05ecad0db5ed058e
Marcel Ritter <marcel+it+github@linux-ng.de>
2025-02-03 13:51:13 +0100

    Added GIT_BUILD_TAG option for web-library

--- web-library/build.sh
+++ web-library/build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 set -x
+set -e
 
 [ -n "${API_AUTHORITY_PART}" ] || exit 1
 [ -n "${WEB_SERVER_URL}" ] || exit 1  
 [ -n "${STREAM_SERVER_ADDRESS}" ] || exit 1
 [ -n "${DATA_SERVER_URL}" ] || exit 1
 
+node -v
+npm -v
+
 # Configure
 BUILD_DIR=/tmp/build
+# Use GIT_BUILD_TAG if set
+GIT_OPTIONS=${GIT_BUILD_TAG:+--branch $GIT_BUILD_TAG}
 rm -rf "${BUILD_DIR}"
-git clone --recursive "https://github.com/zotero/web-library.git" "${BUILD_DIR}"
+git clone ${GIT_OPTIONS} --recursive "https://github.com/zotero/web-library.git" "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 for PATCH in ${SRC_DIR}/patches/*.patch; do
 	patch -p1 < $PATCH
@@ -25,7 +31,7 @@ sed -i s#"http://zotero.org/"#"${DATA_SERVER_URL}"# src/js/utils.js
 sed -i s#"https?://zotero.org/"#"${DATA_SERVER_URL}"# src/js/utils.js
 cat src/js/utils.js
 
-npm ci && npm cache clean --force
+npm install
 npm run build
 
 cp -rf "${BUILD_DIR}"/build/* ${DST_DIR}
