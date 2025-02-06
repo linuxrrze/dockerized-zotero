59a46b5e6d610f1cd0b668920e8db20fe7cde7d2
Marcel Ritter <marcel+git@linux-ng.de>
2025-02-06 21:29:39 +0100

    Fix linux client build (windows & mac TBD)

--- client/build.sh
+++ client/build.sh
@@ -4,14 +4,15 @@ set -x
 set -e
 
 # Configure
-BUILD_DIR="/tmp/zotero"
-ZOTERO_CONFIG_FILE="$BUILD_DIR/resource/config.js"
+SOURCE_DIR="/tmp/zotero"
+BUILD_DIR=$(mktemp -d)
+ZOTERO_CONFIG_FILE="$SOURCE_DIR/resource/config.js"
 # Use GIT_BUILD_TAG if set
 GIT_BRANCH=${ZOTERO_CLIENT_GIT_BUILD_TAG:-main}
 GIT_OPTIONS=${ZOTERO_CLIENT_GIT_BUILD_TAG:+--branch $ZOTERO_CLIENT_GIT_BUILD_TAG}
-git clone ${GIT_OPTIONS} --recursive https://github.com/zotero/zotero "${BUILD_DIR}"
+git clone ${GIT_OPTIONS} --recursive https://github.com/zotero/zotero "${SOURCE_DIR}"
 
-cd "$BUILD_DIR"
+cd "$SOURCE_DIR"
 BRANCH_HASH=$(git log -n 1 --pretty=format:"%H")
 
 
@@ -39,18 +40,44 @@ sed -i s+'^ssh '+'#ssh '+ app/scripts/check_requirements
 sed -i s+'^aws '+'#aws '+ app/scripts/check_requirements
 sed -i s+'^DEPLOY_CMD="ssh'+'DEPLOY_CMD="echo ssh'+ app/config.sh
 
-rm -rf /staging/build
-mkdir /staging/build
-"$BUILD_DIR/app/scripts/fetch_mar_tools"
-"$BUILD_DIR/app/scripts/prepare_build" -s "$BUILD_DIR" -o /staging/build -c release -m "${BRANCH_HASH}"
-"$BUILD_DIR/app/scripts/build_and_deploy" -d /staging/build -p $PLATFORM -c release
-#"$BUILD_DIR/app/scripts/7.0_release_build_and_deploy"
-#"$BUILD_DIR/app/scripts/dir_build" -q $PARAMS -p $PLATFORM
+# Change config
+sed -i s+'^DEPLOY_PATH=.*'+'DEPLOY_PATH="/output/deploy"'+ "$SOURCE_DIR/app/config.sh"
+cat "$SOURCE_DIR/app/config.sh"
+
+sed -i s+'\$DEPLOY_HOST:'+''+ "$SOURCE_DIR/app/scripts/build_and_deploy"
+#sed -i s+'scp '+'echo scp '+ "$SOURCE_DIR/app/scripts/build_and_deploy"
+#sed -i -E s+'scp (.*) (.*)'+'test -f \1 \&\& cp \1 \2 || touch \2'+p "$SOURCE_DIR/app/scripts/build_and_deploy"
+sed -i -E s+'scp (.*) (.*)'+'test -f \1 \&\& cp \1 \2 || cat \2 \&\& echo [] > \2 \&\& cat \2'+p "$SOURCE_DIR/app/scripts/build_and_deploy"
+
+# Disable version_info for now
+#sed -i -E s+'.*add_version_info.*'+'#\0'+ "$SOURCE_DIR/app/scripts/build_and_deploy"
+cat "$SOURCE_DIR/app/scripts/build_and_deploy"
+
+# Enable debug output
+sed -i '2i set -x' "$SOURCE_DIR/app/scripts/build_and_deploy"
+sed -i '2i set -x' "$SOURCE_DIR/app/scripts/upload_builds"
+#sed -i s+'^aws '+'echo aws '+ "$SOURCE_DIR/app/scripts/upload_builds"
+
+# New try
+sed -i s+'^url=.*'+'url=/output/s3/client'+ "$SOURCE_DIR/app/scripts/upload_builds"
+sed -i s+'^aws s3 sync '+'rsync '+ "$SOURCE_DIR/app/scripts/upload_builds"
+
+# Disable scripts
+sed -i '2i exit 0' "$SOURCE_DIR/app/scripts/manage_incrementals"
+
+#rm -rf /output/deploy
+mkdir -p /output/deploy/release
+mkdir -p /output/s3/client
+
+"$SOURCE_DIR/app/scripts/fetch_mar_tools"
+"$SOURCE_DIR/app/scripts/prepare_build" -s "$SOURCE_DIR" -o ${BUILD_DIR} -c release -m "${BRANCH_HASH}"
+"$SOURCE_DIR/app/scripts/build_and_deploy" -d ${BUILD_DIR} -p $PLATFORM -c release
+
+#find .
 
 if [ "`uname`" = "Darwin" ]; then
 	# Sign the Word dylib so it works on Apple Silicon
-	"$BUILD_DIR/app/scripts/codesign_local" "$BUILD_DIR/app/staging/Zotero.app"
+	"$SOURCE_DIR/app/scripts/codesign_local" "$SOURCE_DIR/app/staging/Zotero.app"
 fi
 
-cp -r -f $BUILD_DIR/app/staging/* /staging
-cp -r -f $BUILD_DIR/app/dist/* /dist
+rsync -avx "$SOURCE_DIR"/app/dist/ /output/dist/
